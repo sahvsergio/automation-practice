165d017674b511b5dae11972adb3a792a6b2eff4
savhsergio <sergioandresherrera@gmail.com>
2023-09-09 11:21:29 -0500

    initial commit

---
+++ AUTOMATE_EXCEL.py
@@ -0,0 +1,24 @@
+import csv
+import os
+
+def set_up():
+    try:(os.uname())
+
+    
+
+def create_directory():
+    os.mkdir(practice)
+
+def   
+#enter the directo
+for (root, dirs, files) in os.walk('/'):
+    
+    for file in files:
+        if file.endswith('.csv'):
+            print(file)(
+        elif file.endswith('pdf.'):
+       
+
+print(os.getcwd())
+#read csv
+#csv.reader('')()
